582e8fd375bd2faac1091354a1317b797cb3f0c5
Luiz Fellipe Abrantes <luizfellipeabrantes@gmail.com>
2023-11-29 14:34:38 -0300

    Conflitos resolvidos

--- Moveis-LD/My Project/Application.Designer.vb
+++ Moveis-LD/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Moveis_LD.Form1
+            Me.MainForm = Global.Moveis_LD.frm_login
         End Sub
     End Class
 End Namespace
